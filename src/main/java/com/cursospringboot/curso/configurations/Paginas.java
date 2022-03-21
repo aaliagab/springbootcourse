f90c12181811d933361ae3dfdfdb3c92e0eec62e
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-03-20 20:39:49 -0400

    ModelAndView
    ejemplo de posts con ModelAndView y creando paquete configurations con clase Paginas

---
+++ src/main/java/com/cursospringboot/curso/configurations/Paginas.java
@@ -0,0 +1,5 @@
+package com.cursospringboot.curso.configurations;
+
+public class Paginas {
+	public static String HOME = "index";
+}
--- src/main/java/com/cursospringboot/curso/controllers/ControllerBasic.java
+++ src/main/java/com/cursospringboot/curso/controllers/ControllerBasic.java
@@ -8,17 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.cursospringboot.curso.configurations.Paginas;
 import com.cursospringboot.curso.models.Post;
 
 @Controller
 @RequestMapping("/home")
 public class ControllerBasic {
-	@GetMapping(path = {"/post",""})
-	public String saludar(Model model) {
-		model.addAttribute("posts", this.posts());
-		return "index";
-	}
 	
 	public List<Post> posts(){
 		List<Post> postss = new ArrayList<>();
@@ -31,4 +28,20 @@ public class ControllerBasic {
 		return postss;
 		
 	}
+	
+	@GetMapping(path = {"/post",""})
+	public String saludar(Model model) {
+		model.addAttribute("posts", this.posts());
+		return "index";
+	}
+	
+	//ejemplo igual a saludar pero con ModelAndView
+	@GetMapping(path = {"/public"})
+	public ModelAndView saludarMV() {
+		ModelAndView mv = new ModelAndView(Paginas.HOME);
+		mv.addObject("posts", this.posts());		
+		return mv;
+	}
+	
+	
 }
