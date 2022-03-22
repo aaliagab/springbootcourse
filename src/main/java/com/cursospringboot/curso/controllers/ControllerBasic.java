d2bf5a020eac00e49b9082ba813c17a7dc3caa16
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-03-21 21:15:28 -0400

    post con id url bonita

--- src/main/java/com/cursospringboot/curso/controllers/ControllerBasic.java
+++ src/main/java/com/cursospringboot/curso/controllers/ControllerBasic.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -49,6 +50,19 @@ public class ControllerBasic {
 	@GetMapping(path = {"/post"})
 	public ModelAndView getPostIndividual(
 			@RequestParam(defaultValue = "1", name = "id", required = false) int id
+			) {
+		ModelAndView mv = new ModelAndView(Paginas.POST);		
+		List<Post> listaFiltrada = this.posts().stream().filter(
+				(p)->{return p.getId()==id;}
+				).collect(Collectors.toList());
+		
+		mv.addObject("post", listaFiltrada.get(0));
+		return mv;
+	}
+	
+	@GetMapping(path = {"/post/{idpost}"})
+	public ModelAndView getPostIndividualElegante(
+			@PathVariable(name = "idpost", required = true) int id
 			) {
 		ModelAndView mv = new ModelAndView(Paginas.POST);		
 		List<Post> listaFiltrada = this.posts().stream().filter(
